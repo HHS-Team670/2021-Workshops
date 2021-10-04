d95e9d7d230577363dc70065efca2304e69a0853
riya <riya.zingade@gmail.com>
2021-10-04 16:04:26 -0700

    Create runIntake.java

---
+++ Minibots/runIntake.java
@@ -0,0 +1,34 @@
+package Minibots;
+
+public class runIntake extends CommandBase {
+    
+    //FIELDS
+    private boolean reversed;
+    private Intake intake;
+
+    //CONSTRUCTOR
+
+    public RunIntake(boolean reversed, Intake intake){
+       this.reversed = reversed;
+       this.intake = intake;
+       AddRequirements(intake);
+    }
+
+    public void initialize(){
+
+    }
+
+    public void execute(){
+        while
+    }
+    
+    public void isFinished(){
+
+    }
+
+    public void end(){
+        if (isFinished = true){
+            end();
+        }
+    }
+}
