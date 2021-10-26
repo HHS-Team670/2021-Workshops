8daad5332c58e66b6207f95267aa747c286a04a8
Marius Yen <myen419@student.fuhsd.org>
2021-10-26 16:49:38 -0700

    10/26

--- Minibots/PIDDistanceDrive.java
+++ Minibots/PIDDistanceDrive.java
@@ -32,30 +32,49 @@ public class PIDDistanceDrive extends CommandBase {
 	 * 
 	 * @param target Target distance in inches
 	 */
-	public PIDDistanceDrive() {
+	public PIDDistanceDrive(double targetInches, Drivebase drivebase) {
+
+		driveBase
+
+		m_targetInches = targetInches;
 		
 		double toleranceTicks = MathUtils.convertInchesToEncoderTicks(TOLERANCE_INCHES);
 		
+		m_leftController = new PIDController(kP, kI, kD);
+
+		m_rightController = new PIDController(kP, kI, kD);
+
         m_leftController.setTolerance(toleranceTicks);
 
         m_rightController.setTolerance(toleranceTicks);
+
+		addRequirements(driveBase);
 		
 	}
-
-	public void initialize() {
-	    
-		
+	public void initialize () {
+		double toleranceTicks = MathUtils.convertInchesToEncoderTicks(m_targetInches);
+		m_leftController.setSetpoint(m_targetInches);
+		m_rightController.setSetpoint(m_targetInches);
 	}
 
 	public void execute() {
-	    
+		public Encoder getLeftEncoder() {
+			// TODO Auto-generated method stub
+			return this.le;
+		}
+	
+		public Encoder getRightEncoder() {
+			return this.re;
+		}
+	    driveBase(m_leftController.calculate(m_rightController.getTicks, targetInches(m_rightController.calculate(m_leftController.getTicks, targetInches))
+
 	}
 
 	public boolean isFinished() {
-        return (  ||  );
+        return (m_leftController.setPoint()|| m_rightController.setPoint());
 	}
 
 	public void end(boolean isFinished) {
-		
+		driveBase.stop();
 	}
 }
