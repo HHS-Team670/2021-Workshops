80641490dc3e91dcadd814d119d00b60b2eed7a0
aaditya_raj <89165734+araj754@users.noreply.github.com>
2021-10-26 16:22:05 -0700

    PIDDistanceDrive start code

--- Minibots/PIDDistanceDrive.java
+++ Minibots/PIDDistanceDrive.java
@@ -32,30 +32,53 @@ public class PIDDistanceDrive extends CommandBase {
 	 * 
 	 * @param target Target distance in inches
 	 */
-	public PIDDistanceDrive() {
+	public PIDDistanceDrive(double targetInches, Drivebase drivebase) {
+
+		m_targetInches = targetInches;		
 		
+		
+		m_leftController = new PIDController(kP, kI, kD);
+		m_rightController = new PIDController(kP, kI, kD);
+
 		double toleranceTicks = MathUtils.convertInchesToEncoderTicks(TOLERANCE_INCHES);
-		
+
+
         m_leftController.setTolerance(toleranceTicks);
 
         m_rightController.setTolerance(toleranceTicks);
+
+		addRequirements(driveBase);
 		
 	}
 
 	public void initialize() {
 	    
+		// set the setpoint on the pid controllers with target inches
+		double m_targetTicks = MathUtils.convertInchesToEncoderTicks(m_targetInches);
+
+		// look at documentation for a method to set setpoint on both PID controllers
+
 		
 	}
 
 	public void execute() {
 	    
+		// find the error in ticks using drivebase encoders
+
+		// look at documentation for a method to calculate the new speed with this error
+
+		// call tankDrive with these new speeds
+
 	}
 
 	public boolean isFinished() {
+
+		// documentation has method for checking if the PID controller has reached the setpoint
+
         return (  ||  );
 	}
 
-	public void end(boolean isFinished) {
-		
+	public void end(boolean isInterrupted) {
+		driveBase.stop();
 	}
 }
