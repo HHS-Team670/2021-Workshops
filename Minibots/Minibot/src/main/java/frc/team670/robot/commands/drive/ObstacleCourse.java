0280a2419e17284663d759acf3fd02b6cfffcfc1
aaditya_raj <89165734+araj754@users.noreply.github.com>
2021-11-03 16:06:25 -0700

    obstaclecourse setup

---
+++ Minibots/Minibot/src/main/java/frc/team670/robot/commands/drive/ObstacleCourse.java
@@ -0,0 +1,15 @@
+package frc.team670.robot.commands.drive;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team670.robot.subsystems.DriveBase;
+
+public class ObstacleCourse extends SequentialCommandGroup{
+    
+    public ObstacleCourse(DriveBase driveBase) {
+        addCommands(
+            // add commands here, comma separated!
+        );
+    }
+
+
+}
